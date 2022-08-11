bed414bd4af7f2464619cae9bfd677c6f2850bfc
Ogaga <iyaraogaga69@gmail.com>
2022-08-11 16:33:58 +0100

    Add table references

--- db/migrate/20220811140357_add_likes_ref_to_posts.rb
+++ db/migrate/20220811140357_add_likes_ref_to_posts.rb
@@ -1,5 +1,5 @@
 class AddLikesRefToPosts < ActiveRecord::Migration[7.0]
   def change
-    add_reference :posts, :likescounter, null: false, foreign_key: true
+    add_reference :likes, :post, null: false, foreign_key: true
   end
 end
