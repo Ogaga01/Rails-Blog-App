9e6204d58293529abee39d05e1d9dbd48f63ca53
Ogaga <iyaraogaga69@gmail.com>
2022-08-12 09:42:29 +0100

    Add likes model tests

---
+++ spec/models/like_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe Like, type: :model do
+  user = User.new(name: 'Klaus', photo: 'Mikelson.png', bio: 'Always and forever', post_counter: 0)
+  user.save
+  post = Post.new(user_id: user, title: 'Hello Elijah', text: 'How is mum')
+  post.save
+
+  user.likes.create(post_id: post, user_id: user)
+
+  it 'update likes counter' do
+    expect(user.likes.length).to eq 1
+  end
+end
