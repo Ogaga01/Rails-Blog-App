31a4d0fe875d5710accf8cf485f9b9e83ae14da2
Ogaga <iyaraogaga69@gmail.com>
2022-08-18 08:18:03 +0100

    Update posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,18 @@
 class PostsController < ApplicationController
   def index
-    @user = User.find(params[:user_id])
-    @posts = @user.posts
+    @user = User.find_by(id: params[:user_id])
+    @posts = current_user.posts.includes(:comments)
   end
 
   def show
-    @post = Post.find(params[:id])
+    @user = User.find_by(id: params[:user_id])
+    @post = Post.find_by(id: params[:id])
     @comments = @post.comments
     @likes = @post.likes
+    @new_comment = @post.comments.new
+  end
+
+  def new
+    @user = current_user
   end
 end
