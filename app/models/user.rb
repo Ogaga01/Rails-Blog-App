e3b84fd6c4f6d878ba6429a232b19cf9a1eb22ed
Ogaga <iyaraogaga69@gmail.com>
2022-08-24 13:58:06 +0100

    Add seeds

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,10 @@ class User < ApplicationRecord
   has_many :comments
   has_many :likes
 
-  validates :name, presence: true
-  validates :post_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :name, presence: true, allow_blank: true
+
+  validates :post_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0, allow_nil: true
+ }
 
   def most_recent_posts
     posts.order(created_at: :desc).limit(3)
